374d5f4feb84ae1bc6c2c8fe1da3015dab1d7358
Lochie Westfall <lochiewestfall@gmail.com>
2019-02-22 15:48:28 +1100

    Add convergent hill climbing algorithm within 'random search.py'

--- Cartpole Algorithms/random search.py
+++ Cartpole Algorithms/random search.py
@@ -2,7 +2,7 @@ import gym
 import tensorflow as tf
 
 
-def create_neural_network (net_input, structure, name):
+def create_neural_network(net_input, structure, name):
     with tf.variable_scope(name):
         out = net_input
         for i in range(1, len(structure)):
@@ -14,20 +14,17 @@ def create_neural_network (net_input, structure, name):
 
 env = gym.make('CartPole-v0')
 
-network_shape = ((4), (1, tf.nn.relu))
-print(network_shape[0])
-input = tf.placeholder(tf.float32, [None, network_shape[0]])
+network_shape = (4, (1, None))
 
-_, best_neural_net_vars = create_neural_network(input, network_shape, "best_net")
-neural_net, neural_net_vars = create_neural_network(input, network_shape, "test_net")
+state_input = tf.placeholder(tf.float32, [None, network_shape[0]])
 
-for var in neural_net_vars:
-    print(var.value)
+_, best_neural_net_vars = create_neural_network(state_input, network_shape, "best_net")
+neural_net, neural_net_vars = create_neural_network(state_input, network_shape, "test_net")
 
 update_current_variables = [current.assign(best) for current, best in zip(neural_net_vars, best_neural_net_vars)]
 update_best_variables = [best.assign(current) for current, best in zip(neural_net_vars, best_neural_net_vars)]
 
-mutate_network_variables = [variable.assign(variable + (tf.random_uniform(tf.shape(variable))-0.5) * 0.01) for variable in neural_net_vars]
+mutate_network_variables = [variable.assign(variable + (tf.random_uniform(tf.shape(variable)) * 2 - 1)) for variable in neural_net_vars]
 randomize_network_variables = [variable.assign(tf.random_uniform(tf.shape(variable))) for variable in neural_net_vars]
 
 sess = tf.Session()
@@ -36,27 +33,30 @@ sess.run(tf.initializers.global_variables())
 
 best_reward = 0
 
-episodes = 100
+episodes = 1000
 
 for episode in range(episodes):
 
-    if best_reward == 200:
-        sess.run(update_current_variables)
-    else:
+    if best_reward < 0:
         sess.run(randomize_network_variables)
+    else:
+        sess.run(update_current_variables)
+        sess.run(mutate_network_variables)
+
+    episode_reward = 0
 
     done = False
     state = env.reset()
-    episode_reward = 0
 
     while not done:
         env.render()
-        action = sess.run(neural_net, {input: [state]})[0][0]
-        state, reward, done, info = env.step(int(round(action)))
+        action = sess.run(neural_net, {state_input: [state]})[0][0]
+        state, reward, done, info = env.step(1 if action > 0 else 0)
         episode_reward += reward
 
-    if episode_reward > best_reward:
-        print("Reached", episode_reward, "score in episode", episode + 1)
+    print(episode, ": ", episode_reward)
+    if episode_reward >= best_reward:
         best_reward = episode_reward
         sess.run(update_best_variables)
+
 
